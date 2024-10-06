d02bbac977a7441c54e80a78522bbb310f7a6df0
Baelak <teklemichaelkaleab@gmail.com>
2024-10-06 10:00:57 -0400

    test with connection file

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,7 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+ sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
